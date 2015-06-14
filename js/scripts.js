6305179d6642477df82acc934cbcf1ddec771a80
Nachesi <nachesi@msn.com>
2015-06-14 01:35:31 -0300

    new albuns and update old albuns

--- js/scripts.js
+++ js/scripts.js
@@ -195,48 +195,33 @@ function enableJCarousel () {
     });
 }
 
-function submitLoginForm() {
 
-    var email = $('#loginEmail').val();
-    var pass = $('#loginPassword').val();
-
-    $.post( "../../models/script_login.php", { email: email, password: pass })
-      .done(function( data ) {
-        //alert( "Data Loaded: -" + data + "-" );
-        if(data == "true")
-        {
-            simpleNavigate("views/mainpage.html");
+$(function(){
+// part 1
+        $('menu a').click(function(){
+            location.hash=$(this).attr('href').match(/(^.*)\./)[1]
+            return false
+        })
+// part 2
+        var originalTitle=document.title
+        function hashChange(){
+            var page=location.hash.slice(1)
+            if (page!=""){
+                $('#content').load(page+".html #sub-content")
+                document.title=originalTitle+' – '+page
+            }
         }
-        if(data == "false")
-        {
-            alert("E-mail ou senha incorretos!");
-            simpleNavigate("views/login.html");
+// part 3
+        if ("onhashchange" in window){ // cool browser
+            $(window).on('hashchange',hashChange).trigger('hashchange')
+        }else{ // lame browser
+            var lastHash=''
+            setInterval(function(){
+                if (lastHash!=location.hash)
+                    hashChange()
+                lastHash=location.hash
+            },100)
         }
-      })
+    })
+   
 
-
-
-}
-
-function submitRegisterForm() {
-
-    var email = $('#registerEmail').val();
-    var pass = $('#registerPassword').val();
-    var name = $('#registerName').val();
-    var type = $('#registerType').val();
-
-    $.post( "../../models/script_registration.php", { email: email, password: pass, name: name, type: type })
-      .done(function( data ) {
-        //alert( "Data Loaded: -" + data + "-" );
-        if(data == "true")
-        {
-            simpleNavigate("views/mainpage.html");
-        }
-        if(data == "false")
-        {
-            alert("Ocorreu um problema, tente novamente!");
-            simpleNavigate("views/register.html");
-        }
-      })
-
-}
